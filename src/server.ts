946e415e0495dcd091cd13c0465e4302c7e99c44
Guilherme Teixeira <guilherme.augusto23@hotmail.com>
2023-07-13 20:15:14 -0300

    chore: Preparing app for deploy

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app
   .listen({
-    port: /* env.PORT */ 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log("HTTP Server running on port 3333");
